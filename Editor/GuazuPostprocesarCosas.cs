dc32bb28b96f8904ac463770eb91c343c3895fb6
Paco Pecera <pacoelayudante@gmail.com>
2018-02-21 20:03:04 -0800

    postprocesar tetura

---
+++ Editor/GuazuPostprocesarCosas.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public static class GuazuPostprocesarCosas {
+
+    public static void TexturaSoloAlfa(string assetPath, Texture2D texture)
+    {
+        for (int m = 0; m < texture.mipmapCount; m++)
+        {
+            Color[] c = texture.GetPixels(m);
+
+            for (int i = 0; i < c.Length; i++)
+            {
+                c[i].r = 1f;
+                c[i].g = 1f;
+                c[i].b = 1f;
+            }
+            texture.SetPixels(c, m);
+        }
+    }
+
+}
