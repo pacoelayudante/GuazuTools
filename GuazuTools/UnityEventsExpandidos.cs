6dda66b1b8625498e71d7bde90983b2e763e7ee7
Paco Pecera <pacoelayudante@gmail.com>
2018-03-01 14:34:37 -0800

    agregado unity in y bool

--- GuazuTools/UnityEventsExpandidos.cs
+++ GuazuTools/UnityEventsExpandidos.cs
@@ -3,4 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class UnityEventFloat : UnityEvent<float> {};
+public class UnityEventFloat : UnityEvent<float> {}
+public class UnityEventBool : UnityEvent<bool> { }
+public class UnityEventInt : UnityEvent<int> { }
