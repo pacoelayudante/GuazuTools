24f6dc4b2b5c2e15f3dc8c8ff52c8348059234aa
Paco Pecera <pacoelayudante@gmail.com>
2018-04-06 13:40:09 -0300

    guazu ayuditas

---
+++ Editor/GuazuAyuditasEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GuazuAyuditasEditor : EditorWindow {
+
+    [MenuItem("Assets/Copiar Path De Asset %w")]
+    public static void CopiarPathDeAsset()
+    {
+        if (Selection.assetGUIDs.Length > 0)
+        {
+            EditorGUIUtility.systemCopyBuffer = AssetDatabase.GUIDToAssetPath(Selection.assetGUIDs[0]);
+        }
+    }
+
+}
